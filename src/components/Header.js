e87397a25f4b01bab180e5d820acb81ac848fcfa
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-07-13 09:11:17 +0330

    logout action dispatched in btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Cart from "./Cart";
 import "./Header.css";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../reducers/authSlice";
+
 const Header = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
   return (
     <header>
       <nav className="header-nav">
@@ -17,6 +22,11 @@ const Header = () => {
           <li>
             <Cart />
           </li>
+          {isLoggedIn && (
+            <li>
+              <button onClick={() => dispatch(logout())}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
