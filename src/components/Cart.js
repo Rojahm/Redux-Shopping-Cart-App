fd0eb539ac518b7468ab4748dc8b8b2b796fff8d
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-07-13 08:55:31 +0330

    toggle showcart in layout

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Cart.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { showCart } from "../reducers/cartSlice";
 
 const Cart = () => {
   const quantity = useSelector((state) => state.cart.totalQuantity);
+  const dispatch = useDispatch();
   return (
-    <div className="cartIcon">
+    <div className="cartIcon" onClick={() => dispatch(showCart())}>
       <h3>Cart: {quantity} Items</h3>
     </div>
   );
