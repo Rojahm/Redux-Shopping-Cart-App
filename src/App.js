0882019847601f211c3cffdd8b91fd4e44be1b09
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-07-14 14:37:40 +0330

    cart create real time data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
@@ -6,8 +6,16 @@ import { useSelector } from "react-redux";
 
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const cartItems = useSelector((state) => state.cart.itemsList);
-  console.log(cartItems);
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    fetch(
+      "https://redux-http-79d24-default-rtdb.europe-west1.firebasedatabase.app/cartItems.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
