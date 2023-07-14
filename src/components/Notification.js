81d811018957eaec6f62c21e38e01249abd6d791
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-07-14 22:07:50 +0330

    notification component

---
+++ src/components/Notification.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Alert } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { showNotification } from "../reducers/notificationSlice";
+
+const Notification = ({ type, message }) => {
+  const dispatch = useDispatch();
+  const handleClose = () => {
+    dispatch(showNotification({ open: false }));
+  };
+  const open = useSelector((state) => state.notification.open);
+  console.log(open);
+  return (
+    <div>
+      <br />
+      {open && (
+        <Alert onClose={handleClose} severity={type}>
+          {message}
+        </Alert>
+      )}
+    </div>
+  );
+};
+
+export default Notification;
