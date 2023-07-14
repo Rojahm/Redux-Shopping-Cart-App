ddabb70f1b55ed2494124deca913ca93598037a1
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-07-14 18:59:37 +0330

    made fetch async

--- src/App.js
+++ src/App.js
@@ -8,13 +8,17 @@ function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const cart = useSelector((state) => state.cart);
   useEffect(() => {
-    fetch(
-      "https://redux-http-79d24-default-rtdb.europe-west1.firebasedatabase.app/cartItems.json",
-      {
-        method: "PUT",
-        body: JSON.stringify(cart),
-      }
-    );
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://redux-http-79d24-default-rtdb.europe-west1.firebasedatabase.app/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await response.json();
+    };
+    sendRequest();
   }, [cart]);
   return (
     <div className="App">
